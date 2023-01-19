340578e1d351317e8dccfdef33767c28b3195459
Albina <albisha900325@gmail.com>
2023-01-19 22:18:41 +0500

    27

--- zadacha027/Program.cs
+++ zadacha027/Program.cs
@@ -8,6 +8,15 @@ Console.WriteLine("Введите число: ");
 
 int n = Convert.ToInt32(Console.ReadLine());
 
+int sum = 0;
+
+while ( n>0)
+{
+    sum+=n%10;  
+    n/=10; 
+}
+
+Console.WriteLine(sum);
 
 
 
