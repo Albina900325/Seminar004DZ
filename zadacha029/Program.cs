ae5152e21432aa4490a45128b19866badb01ea72
Albina <albisha900325@gmail.com>
2023-01-19 21:53:41 +0500

    29

---
+++ zadacha029/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов, которые вводит пользователь,
+//и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33];
+
+Console.Clear();
+Console.WriteLine("Введите числа: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int n1 = Convert.ToInt32 (Console.ReadLine());
+int n2 = Convert.ToInt32 (Console.ReadLine());
+int n3 = Convert.ToInt32 (Console.ReadLine());
+int n4 = Convert.ToInt32 (Console.ReadLine());
+int n5 = Convert.ToInt32 (Console.ReadLine());
+int n6 = Convert.ToInt32 (Console.ReadLine());
+int n7 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine($"[{n},{n1},{n2},{n3},{n4},{n5},{n6},{n7}]");
+
